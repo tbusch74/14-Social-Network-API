1a2ab857a6215b8bf675a7a8296e472f3411158f
Tony <abusch@css.edu>
2022-01-13 19:48:51 -0600

    create models

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model, Types } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        thoughts: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+            }
+        ],
+        friends: [UserSchema]
+    },
+    {
+        toJSON: {
+          virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
